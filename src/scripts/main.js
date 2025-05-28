e05aee1ae901759dda3e64df67b0dd618e6536ef
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-05-28 13:53:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // write your code here
+
 const tableBody = document.querySelector('tbody');
+
 const tableHead = document.querySelector('thead');
 const tableFoot = document.querySelector('tfoot');
 const headName = tableHead.rows[0].cells[1].innerHTML;
 const footName = tableFoot.rows[0].cells[1].innerHTML;
-const lasthead = document.createElement('tr');
-const lastfoot = document.createElement('tr');
+const lasthead = tableHead.children[0].children[4].cloneNode(true);
+const lastfoot = tableFoot.children[0].children[4].cloneNode(true);
 
 lasthead.innerHTML = headName;
 lastfoot.innerHTML = footName;
