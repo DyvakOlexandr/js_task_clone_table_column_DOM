9f9c60670bb64d3efd756171df644900d5f01db5
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-05-28 12:50:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write your code here
+const tableBody = document.querySelector('tbody');
+const tableHead = document.querySelector('thead');
+const tableFoot = document.querySelector('tfoot');
+const headName = tableHead.rows[0].cells[1].innerHTML;
+const footName = tableFoot.rows[0].cells[1].innerHTML;
+const lasthead = document.createElement('tr');
+const lastfoot = document.createElement('tr');
+
+lasthead.innerHTML = headName;
+lastfoot.innerHTML = footName;
+
+const elementHeadTooAdd = tableHead.children[0].children[4];
+
+elementHeadTooAdd.before(lasthead);
+
+const elementFootTooAdd = tableFoot.children[0].children[4];
+
+elementFootTooAdd.before(lastfoot);
+
+for (const row of tableBody.children) {
+  const salaryCell = row.children[4];
+  const newCell = document.createElement('td');
+
+  newCell.textContent = row.children[1].textContent;
+  salaryCell.before(newCell);
+}
